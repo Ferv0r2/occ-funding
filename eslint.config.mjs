256a5ae024adc516820091b178fd993feb642238
Ferv0r2 <amlk31255@gmail.com>
2025-01-04 18:39:50 +0900

    Feat: Initialize Prettier & ESLint (#1)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import importPlugin from 'eslint-plugin-import';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,42 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.config({
+    extends: ['next', 'prettier'],
+  }),
+  {
+    files: ['**/*.{ts,tsx}'],
+    ...importPlugin.flatConfigs.typescript,
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          pathGroups: [
+            { pattern: 'next', group: 'builtin' },
+            { pattern: 'react', group: 'builtin' },
+            { pattern: '@/components/**', group: 'internal' },
+            { pattern: '@/hooks/**', group: 'internal' },
+            { pattern: '@/util/**', group: 'internal' },
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/no-unresolved': 'error',
+      'import/no-duplicates': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
