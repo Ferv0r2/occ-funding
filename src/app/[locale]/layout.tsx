5e647025165ba5a5db009b03f29a73c05ac5d76a
Ferv0r2 <amlk31255@gmail.com>
2025-01-05 20:11:06 +0900

    Feat: Add next-intl & example code (#3)

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,31 @@
+import { notFound } from 'next/navigation';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { routing } from '@/i18n/routing';
+
+type Params = Promise<{ locale: never }>;
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Params;
+}) {
+  const { locale } = await params;
+  // https://nextjs.org/docs/messages/sync-dynamic-apis
+
+  if (!routing.locales.includes(locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <NextIntlClientProvider messages={messages}>
+        <body>{children}</body>
+      </NextIntlClientProvider>
+    </html>
+  );
+}
---
+++ src/i18n/routing.ts
@@ -0,0 +1,15 @@
+import { createNavigation } from 'next-intl/navigation';
+import { defineRouting } from 'next-intl/routing';
+
+export const routing = defineRouting({
+  locales: ['ko', 'en'],
+  defaultLocale: 'ko',
+});
+
+/**
+ * If you want i18n routing, use this instead of `next/link`.
+ * ❌ import Link from 'next/link';
+ * ✅ import { Link, redirect, usePathname, useRouter } from '@/i18n/routing';
+ */
+export const { Link, redirect, usePathname, useRouter } =
+  createNavigation(routing);
