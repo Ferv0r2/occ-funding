e7a70fc402ac70fc9543d2e731256874dcfedf3d
Ferv0r2 <amlk31255@gmail.com>
2025-01-05 20:55:15 +0900

    Feat: Self-review - Code convention (#3)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
+import { ReactNode } from 'react';
 import { routing } from '@/i18n/routing';
 
 type Params = Promise<{ locale: never }>;
@@ -9,7 +10,7 @@ export default async function LocaleLayout({
   children,
   params,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
   params: Params;
 }) {
   const { locale } = await params;
