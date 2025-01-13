7cede82b02b5a8a7e6dff3ff25a7a10829ce87c0
Ferv0r2 <amlk31255@gmail.com>
2025-01-13 23:08:05 +0900

    Feat: Update header with Profile (#13)

---
+++ src/components/items/UserProfile.tsx
@@ -0,0 +1,68 @@
+import { User } from 'lucide-react';
+import { useTranslations } from 'next-intl';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { replaceAddress } from '@/lib/utils/replace-data';
+
+interface UserProfileProps {
+  isLoading: boolean;
+  name: string;
+  email: string;
+  walletAddress: string;
+  profileImage?: string;
+}
+
+export const UserProfile = ({
+  isLoading,
+  name,
+  email,
+  walletAddress,
+  profileImage,
+}: UserProfileProps) => {
+  const t = useTranslations('Header');
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src={profileImage} alt={name} />
+            <AvatarFallback>
+              <User />
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{name}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {!isLoading && (
+          <DropdownMenuItem>
+            {t('wallet_info', {
+              address: replaceAddress(walletAddress),
+            })}
+          </DropdownMenuItem>
+        )}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>{t('profile')}</DropdownMenuItem>
+        <DropdownMenuItem>{t('settings')}</DropdownMenuItem>
+        <DropdownMenuItem>{t('logout')}</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
